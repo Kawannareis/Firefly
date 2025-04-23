79355cd47784578b6e5e36fd543ff9acae299ca0
Alan2088 <alangviana7@gmail.com>
2025-04-23 15:10:08 -0300

    adição do backend

---
+++ agenda-contatos-backend/agenda-contatos-backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',         
+    password: '',         
+    database: 'agenda'
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('MySQL conectado!');
+});
+
+app.post('/contatos', (req, res) => {
+    const { nome, numero, email } = req.body;
+    const sql = 'INSERT INTO contatos (nome, numero, email) VALUES (?, ?, ?)';
+    db.query(sql, [nome, numero, email], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).send({ message: 'Contato adicionado com sucesso!' });
+    });
+});
+
+app.get('/contatos', (req, res) => {
+    db.query('SELECT * FROM contatos', (err, results) => {
+        if (err) return res.status(500).send(err);
+        res.json(results);
+    });
+});
+
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
